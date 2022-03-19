f1b916f1dc65d0e758e0ba1df3b936eb5ce0cfa9
Ylan <99525156+YlanNaly@users.noreply.github.com>
2022-03-19 07:55:48 +0300

    add index.php

---
+++ solaire/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="main.css">
+  <title>Orbit</title>
+</head>
+
+<body id="universe">
+  <div id="stars"></div>
+  <div id="sun"></div>
+  <div id="earthOrbit">
+    <img src="/images/earth.png" alt="" id="earth">
+    <div id="moonOrbit">
+      <div id="moon"></div>
+    </div>
+  </div>
+
+
+
+
+</body>
+
+</html>
